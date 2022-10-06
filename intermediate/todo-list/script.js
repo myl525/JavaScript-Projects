1eccf3f357b005d9028b0f8607412ff10a64681d
Mingyang Liu <castielliu525@gmail.com>
2022-10-06 19:58:14 -0400

    finish

--- intermediate/todo-list/script.js
+++ intermediate/todo-list/script.js
@@ -2,35 +2,51 @@ document.addEventListener('DOMContentLoaded', main);
 
 function main(evt) {
     displayDate();
-    
+    window.onclick = function(evt) {
+        if (evt.target === document.getElementById('addTaskModal')) {
+            closeModal();
+        }
+    }
     //check a task
     const todoArea = document.getElementById('todoArea');
     const doneArea = document.getElementById('doneArea');
 
-    const cards = document.querySelectorAll('.card');
-    const radios = document.querySelectorAll('.radio');
-    
-    radios.forEach((radio) => {
-        radio.addEventListener('click', (evt) => {
-            const task = radio.nextElementSibling;
-            if(radio.style.color) {
-                radio.style.color = "";
-                task.style.textDecoration = "";
-                task.style.color = "";
-                todoArea.appendChild(radio.parentElement);
-            }else {
-                radio.style.color = "black";
-                task.style.textDecoration = "line-through";
-                task.style.color = "grey";
-                doneArea.appendChild(radio.parentElement);
-            }
-        })
+    // add a task
+    const openModal = document.getElementById('addTask');
+    openModal.addEventListener('click', (evt) => {
+        const modal = document.getElementById('addTaskModal');
+        if(modal.classList.contains('not-display')) {
+            modal.classList.remove('not-display');
+        }
     })
 
+    const addBtn = document.getElementById('addTaskConfirmBtn');
+    addBtn.addEventListener('click', (evt) => {
+        const newTask = document.getElementById('newTask').value;
+        if(newTask) {
+            const newCard = document.createElement('div');
+            const newCardRadio = document.createElement('button');
+            const checkIcon = document.createElement('iconify-icon');
+            checkIcon.setAttribute('icon', 'dashicons:yes-alt');
+            const newCardTask = document.createElement('div');
+            newCardTask.textContent = newTask;
+            const deleteBtn = document.createElement('button');
+            const deleteIcon = document.createElement('iconify-icon');
+            deleteIcon.setAttribute('icon', 'codicon:trash');
 
-
-
-
+            newCard.className = "card";
+            newCardRadio.className = "radio";
+            newCardTask.className = "task";
+            deleteBtn.className = "delete";
+            newCardRadio.appendChild(checkIcon);
+            handleCheck(newCardRadio);
+            deleteBtn.appendChild(deleteIcon);
+            handleDelete(deleteBtn);
+            newCard.append(newCardRadio, newCardTask, deleteBtn);
+            todoArea.appendChild(newCard);
+        }
+        closeModal();
+    })
 
     function displayDate() {
         const date = document.getElementById('date');
@@ -51,6 +67,38 @@ function main(evt) {
         return parsedDate;
     }
 
+    function handleCheck(radio) {
+        radio.addEventListener('click', (evt) => {
+            const task = radio.nextElementSibling;
+            if(radio.style.color) {
+                radio.style.color = "";
+                task.style.textDecoration = "";
+                task.style.color = "";
+                todoArea.appendChild(radio.parentElement);
+            }else {
+                radio.style.color = "black";
+                task.style.textDecoration = "line-through";
+                task.style.color = "grey";
+                doneArea.appendChild(radio.parentElement);
+            }
+        })
+    }
+
+    function handleDelete(deleteBtn) {
+        deleteBtn.addEventListener('click', (evt) => {
+            deleteBtn.parentElement.remove();
+        })
+    }
+
+    function closeModal() {
+        const modal = document.getElementById('addTaskModal');
+        const newTask = document.getElementById('newTask');
+
+        if(!modal.classList.contains('not-display')) {
+            newTask.value = "";
+            modal.classList.add('not-display'); 
+        }
+    }
 }
 
 
